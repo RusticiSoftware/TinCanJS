5dd4661352434d470e7215ed82c59a6fcc6bb679
Brian J. Miller <brianmiller@residualselfimage.com>
2013-04-21 09:33:24 -0500

    Build 0.4.0

--- build/tincan.js
+++ build/tincan.js
@@ -1004,7 +1004,7 @@ var TinCan;
     TinCan.versions = function () {
         // newest first so we can use the first as the default
         return [
-            "1.0",
+            "1.0.0",
             "0.95",
             "0.9"
         ];
