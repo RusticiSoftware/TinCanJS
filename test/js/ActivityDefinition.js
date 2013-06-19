e8665995e53081fd0584bd0fcc35405ce551850c
Brian J. Miller <brianmiller@residualselfimage.com>
2013-06-19 08:47:13 -0500

    Add basic ActivityDefinition unit tests

--- test/js/ActivityDefinition.js
+++ test/js/ActivityDefinition.js
@@ -14,21 +14,152 @@
     limitations under the License.
 */
 (function () {
-    var session = null;
+    var session = null,
+        commonType = "http://adlnet.gov/expapi/activities/simulation",
+        commonName = { und: "Test Name" },
+        commonDesc = { und: "Test Description" };
 
     module("ActivityDefinition Statics");
 
     test(
         "ActivityDefinition.fromJSON",
         function () {
-            var raw = {
-                },
+            var raw = {},
                 string,
                 result
             ;
 
             result = TinCan.ActivityDefinition.fromJSON(JSON.stringify(raw));
             ok(result instanceof TinCan.ActivityDefinition, "returns TinCan.ActivityDefinition");
+        }
+    );
+
+    module("ActivityDefinition Instance");
+
+    test(
+        "activity definition Object",
+        function () {
+            var obj = new TinCan.ActivityDefinition (),
+                nullProps = [
+                    "name",
+                    "description",
+                    "type",
+                    "extensions",
+                    "interactionType",
+                    "correctResponsesPattern",
+                    "choices",
+                    "scale",
+                    "source",
+                    "target",
+                    "steps"
+                ],
+                i
+            ;
+
+            ok(obj instanceof TinCan.ActivityDefinition, "object is TinCan.ActivityDefinition");
+
+            for (i = 0; i < nullProps.length; i += 1) {
+                ok(obj.hasOwnProperty(nullProps[i]), "object has property: " + nullProps[i]);
+                strictEqual(obj[nullProps[i]], null, "object property initial value: " + nullProps[i]);
+            }
+
+            strictEqual(obj.LOG_SRC, "ActivityDefinition", "object property LOG_SRC initial value");
+        }
+    );
+
+    test(
+        "activity definition variants",
+        function () {
+            var set = [
+                    {
+                        name: "basic properties",
+                        instanceConfig: {
+                            type: commonType,
+                            name: commonName,
+                            description: commonDesc
+                        },
+                        toString: "Test Name",
+                        checkProps: {
+                            type: commonType,
+                            name: commonName,
+                            description: commonDesc
+                        }
+                    },
+                    {
+                        name: "description only for toString",
+                        instanceConfig: {
+                            description: commonDesc
+                        },
+                        toString: "Test Description",
+                        checkProps: {
+                            description: commonDesc
+                        }
+                    }
+                ],
+                i,
+                obj,
+                result
+            ;
+
+            for (i = 0; i < set.length; i += 1) {
+                row = set[i];
+                obj = new TinCan.ActivityDefinition (row.instanceConfig);
+
+                ok(obj instanceof TinCan.ActivityDefinition, "object is TinCan.ActivityDefinition (" + row.name + ")");
+                strictEqual(obj.toString(), row.toString, "object.toString (" + row.name + ")");
+                if (typeof row.checkProps !== "undefined") {
+                    for (key in row.checkProps) {
+                        deepEqual(obj[key], row.checkProps[key], "object property initial value: " + key + " (" + row.name + ")");
+                    }
+                }
+            }
+        }
+    );
+
+    test(
+        "activity definition asVersion",
+        function () {
+            var set = [
+                    {
+                        name: "basic properties",
+                        instanceConfig: {
+                            type: commonType,
+                            name: commonName,
+                            description: commonDesc
+                        },
+                        versions: {
+                            latest: {
+                                type: commonType,
+                                name: commonName,
+                                description: commonDesc
+                            },
+                            "0.9": {
+                                type: "simulation",
+                                name: commonName,
+                                description: commonDesc
+                            }
+                        }
+                    }
+                ],
+                versions = TinCan.versions(),
+                i,
+                v,
+                obj,
+                result
+            ;
+
+            for (i = 0; i < set.length; i += 1) {
+                row = set[i];
+                obj = new TinCan.ActivityDefinition (row.instanceConfig);
+
+                result = obj.asVersion();
+                deepEqual(result, row.versions.latest, "object.asVersion() latest: " + row.name);
+
+                for (v = 0; v < versions.length; v += 1) {
+                    result = obj.asVersion(versions[v]);
+                    deepEqual(result, (row.versions.hasOwnProperty(versions[v]) ? row.versions[versions[v]] : row.versions.latest), "object.asVersion() " + versions[v] + " : " + row.name);
+                }
+            }
         }
     );
 }());
