c6ad07c5c0fe1d9a6f7d2f48f97cc0469e427cc9
Brian J. Miller <brian.miller@scorm.com>
2015-03-03 12:02:20 -0600

    API Doc for 0.33.0

---
+++ doc/api/0.33.0/api.js
@@ -0,0 +1,176 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "TinCan",
+        "TinCan.About",
+        "TinCan.Activity",
+        "TinCan.ActivityDefinition",
+        "TinCan.ActivityProfile",
+        "TinCan.Agent",
+        "TinCan.AgentAccount",
+        "TinCan.AgentProfile",
+        "TinCan.Context",
+        "TinCan.ContextActivities",
+        "TinCan.Group",
+        "TinCan.InteractionComponent",
+        "TinCan.LRS",
+        "TinCan.Result",
+        "TinCan.Score",
+        "TinCan.State",
+        "TinCan.Statement",
+        "TinCan.StatementRef",
+        "TinCan.StatementsResult",
+        "TinCan.SubStatement",
+        "TinCan.Utils",
+        "TinCan.Verb"
+    ],
+    "modules": [
+        "TinCan",
+        "TinCan.About",
+        "TinCan.Activity",
+        "TinCan.ActivityDefinition",
+        "TinCan.ActivityProfile",
+        "TinCan.Agent",
+        "TinCan.AgentAccount",
+        "TinCan.AgentProfile",
+        "TinCan.Context",
+        "TinCan.ContextActivities",
+        "TinCan.Environment.Browser",
+        "TinCan.Environment.Node",
+        "TinCan.Group",
+        "TinCan.InteractionComponent",
+        "TinCan.LRS",
+        "TinCan.Result",
+        "TinCan.Score",
+        "TinCan.State",
+        "TinCan.Statement",
+        "TinCan.StatementRef",
+        "TinCan.StatementsResult",
+        "TinCan.SubStatement",
+        "TinCan.Utils",
+        "TinCan.Verb"
+    ],
+    "allModules": [
+        {
+            "displayName": "TinCan",
+            "name": "TinCan",
+            "description": "TinCan client library\n\nTODO:\n\n* Add statement queueing"
+        },
+        {
+            "displayName": "TinCan.About",
+            "name": "TinCan.About",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Activity",
+            "name": "TinCan.Activity",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.ActivityDefinition",
+            "name": "TinCan.ActivityDefinition",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.ActivityProfile",
+            "name": "TinCan.ActivityProfile",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Agent",
+            "name": "TinCan.Agent",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.AgentAccount",
+            "name": "TinCan.AgentAccount",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.AgentProfile",
+            "name": "TinCan.AgentProfile",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Context",
+            "name": "TinCan.Context",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.ContextActivities",
+            "name": "TinCan.ContextActivities",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Environment.Browser",
+            "name": "TinCan.Environment.Browser",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Environment.Node",
+            "name": "TinCan.Environment.Node",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Group",
+            "name": "TinCan.Group",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.InteractionComponent",
+            "name": "TinCan.InteractionComponent",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.LRS",
+            "name": "TinCan.LRS",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Result",
+            "name": "TinCan.Result",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Score",
+            "name": "TinCan.Score",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.State",
+            "name": "TinCan.State",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Statement",
+            "name": "TinCan.Statement",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.StatementRef",
+            "name": "TinCan.StatementRef",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.StatementsResult",
+            "name": "TinCan.StatementsResult",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.SubStatement",
+            "name": "TinCan.SubStatement",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Utils",
+            "name": "TinCan.Utils",
+            "description": "TinCan client library"
+        },
+        {
+            "displayName": "TinCan.Verb",
+            "name": "TinCan.Verb",
+            "description": "TinCan client library"
+        }
+    ]
+} };
+});
