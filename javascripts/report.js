428373b3fd906a529b7fbb2b8f451be0c0fe9a9f
Brian J. Miller <brian.miller@rusticisoftware.com>
2016-01-05 09:05:29 -0600

    xAPI name stuff, TinCanJS bump

--- javascripts/report.js
+++ javascripts/report.js
@@ -1,27 +1,37 @@
 (function () {
-    var tincan = new TinCan (
-        {
-            recordStores: [
-                {
-                    endpoint: "https://cloud.scorm.com/ScormEngineInterface/TCAPI/public/",
-                    username: "test",
-                    password: "pass"
+    var lrs,
+        statement = new TinCan.Statement(
+            {
+                actor: {
+                    mbox: "mailto:tincanjs-github@tincanapi.com"
+                },
+                verb: {
+                    id: "http://adlnet.gov/expapi/verbs/attempted"
+                },
+                target: {
+                    id: "http://rusticisoftware.github.com/TinCanJS"
                 }
-            ]
-        }
-    );
-    tincan.sendStatement(
-        {
-            actor: {
-                mbox: "mailto:tincanjs-github@tincanapi.com"
-            },
-            verb: {
-                id: "http://adlnet.gov/expapi/verbs/attempted"
-            },
-            target: {
-                id: "http://rusticisoftware.github.com/TinCanJS"
             }
-        },
-        function () {}
+        );
+
+    try {
+        lrs = new TinCan.LRS(
+            {
+                endpoint: "https://cloud.scorm.com/ScormEngineInterface/TCAPI/public/",
+                username: "test",
+                password: "pass",
+                allowFail: false
+            }
+        );
+    }
+    catch (ex) {
+        console.log("Failed to setup LRS object: " + ex);
+    }
+
+    lrs.saveStatement(
+        statement,
+        {
+            callback: function () {}
+        }
     );
 }());
